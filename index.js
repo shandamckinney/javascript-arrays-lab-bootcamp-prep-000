4d87d49bdfaffb9300bd4bad688e64a66255bbf1
shandamckinney <mckinney.shanda@gmail.com>
2018-05-08 18:24:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
 
 window.kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return destructivelyAppendKitten()
+}
