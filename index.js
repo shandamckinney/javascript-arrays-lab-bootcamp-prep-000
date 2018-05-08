fe374fd758c10c5424c7885653229ead64cf5b8c
shandamckinney <mckinney.shanda@gmail.com>
2018-05-08 18:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,3 +46,10 @@ window.kittens = ['Milo', 'Otis', 'Garfield']
 function removeLastKitten(){
   return kittens.slice(0, kittens.length - 1)
 }
+
+window.kittens = ['Milo', 'Otis', 'Garfield']
+
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
